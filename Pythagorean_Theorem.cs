ec6f2a20f118442c68d44c4c0f44275e914ef603
Bob Bass <bob@bbass.co>
2021-03-07 19:23:09 -0500

    finished basic project and added readme

---
+++ Pythagorean_Theorem.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Pythagorean_Theorem
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Stereotypical Pythagorean Theorem console app to brush up on C# syntax\n\n");
+      double a = 0;
+      double b = 0;
+
+      //* Get sides via user input 
+      a = GetSideA();
+      b = GetSideB();
+
+
+      //* Convert sides to double 
+      double numA = Convert.ToDouble(a);
+      double numB = Convert.ToDouble(b);
+
+      consolelog("Side A: " + numA + "\nSide B: " + numB + "\n");
+
+      double numC = CalculateSideC(numA, numB);
+
+      consolelog("Side C is: " + numC.ToString());
+      consolelog("\n\nPress any key to exit");
+
+      //* Stop program from exiting instantly 
+      Console.ReadKey();
+    }
+
+    //* Methods
+    public static double CalculateSideC(Double sideA, Double sideB)
+    {
+      // * Get side c squared
+      Double cSquared = (sideA * sideA) + (sideB * sideB);
+      consolelog("Side C^2: " + cSquared.ToString());
+
+      //* Get sqrt of side c squared
+      Double sideC = Math.Round(Math.Sqrt(cSquared), 2);
+      return sideC;
+    }
+
+    //? To mimic TypeScript for the 🤣's
+    public static void consolelog(String value)
+    {
+      Console.WriteLine(value);
+    }
+
+    public static bool TestInputNum(string input)
+    {
+      //* Test input values for being over 0 and numbers
+      bool doubleTest = double.TryParse(input, out double result);
+      bool amountTest = result > 0;
+      consolelog("doubleTest " + doubleTest + "\namountTest " + amountTest);
+
+      if (doubleTest != true || amountTest != true)
+      {
+        //! Falied test
+        Console.WriteLine("📛 Invalid Entry - Please enter a number \n\n");
+        return false;
+      }
+      else
+      {
+        //* Good, move on
+        Console.WriteLine("✅" + result + " is a valid entry \n\n");
+        return true;
+      }
+    }
+
+    //* Abstracted out code to get the user input. This can be further abstracted to 1 method instead of 2
+    public static double GetSideA()
+    {
+      consolelog("Please enter value for A:");
+      string valA = Console.ReadLine();
+      if (TestInputNum(valA))
+      {
+        return Double.Parse(valA);
+      }
+      else
+      {
+        return GetSideA();
+      }
+    }
+    public static double GetSideB()
+    {
+      consolelog("Please enter value for B:");
+      string valB = Console.ReadLine();
+      if (TestInputNum(valB))
+      {
+        return Double.Parse(valB);
+      }
+      else
+      {
+        return GetSideB();
+      }
+    }
+
+
+
+  }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# c-sharp-syntax-reminder
+# C# SYNTAX REMINDER
+
+## PURPOSE
+
+As a JavaScript developer, I get rusty with my C# after going months on end without using it.
+
+This is the exact app I create every time I need to brush up on my C# syntax.
+
+I'm pushing this to my GitHub because I'd rather just reference it as opposed to re-create it next time.
+
+Feel free to add to it. This is nothing besides a visual syntax reminder with some goofy methods for fun.
+
+## Pythagorean theorem
+
+```latex
+a^2 + b^2 = c^2
+```
+
+## TODO
+
+* FizzBuzz 😉
+
+## Notes
+.NET5
+
+Created in VS Code with WSL2/Ubuntu 20.04
+
+```json
+{
+  "runtimeOptions": {
+    "tfm": "net 5.0",
+    "framework": {
+      "name": "Microsoft.NETCore.App",
+      "version": "5.0.0"
+    }
+  }
+}
+```
